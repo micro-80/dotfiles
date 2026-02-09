7017be3b0bbf3006c02a5a612aba867f55164d61
micro80 <me@micro80.com>
2026-02-09 09:54:15 +0000

    Changes

--- common/.config/nvim/init.lua
+++ common/.config/nvim/init.lua
@@ -1,3 +1,6 @@
+local lsp_ensure_installed = { "lua_ls", "stylua", "vtsls"}
+local treesitter_file_types = { "c", "lua", "typescript", "tsx" }
+
 local set_key = vim.keymap.set
 local keymap_defaults = { noremap = true, silent = true }
 
@@ -9,10 +12,13 @@ vim.o.foldlevel = 99
 vim.o.foldlevelstart = 99
 vim.o.inccommand = "split"
 vim.o.number = true
-vim.o.relativenumber = true
+--vim.o.relativenumber = true
+vim.o.pumborder = "single"
 vim.o.scrolloff = 10
 vim.o.signcolumn = "yes"
 vim.o.termguicolors = true
+
+vim.cmd("colorscheme default")
 
 vim.lsp.config("lua_ls", {
 	settings = {
@@ -24,34 +30,6 @@ vim.lsp.config("lua_ls", {
 	},
 })
 
-set_key("n", "<leader>d", vim.diagnostic.open_float, keymap_defaults)
-set_key("n", "<leader>rn", vim.lsp.buf.rename, keymap_defaults)
-
-vim.pack.add({
-	'https://github.com/mason-org/mason.nvim',
-	'https://github.com/mason-org/mason-lspconfig.nvim',
-	'https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim',
-	"https://github.com/nvim-mini/mini.nvim",
-	"https://github.com/neovim/nvim-lspconfig",
-	"https://github.com/nvim-treesitter/nvim-treesitter",
-	"https://github.com/tpope/vim-sleuth",
-	"https://github.com/vague-theme/vague.nvim",
-})
-
-require("mini.completion").setup()
-require("mini.icons").setup()
-require("mini.notify").setup()
-require("mini.surround").setup()
-
-local mini_pick = require("mini.pick")
-mini_pick.setup()
-set_key("n", "<leader>h", mini_pick.builtin.help, keymap_defaults)
-set_key("n", "\\", mini_pick.builtin.grep_live, keymap_defaults)
-set_key("n", ",", mini_pick.builtin.buffers, keymap_defaults)
-set_key("n", ";", mini_pick.builtin.files, keymap_defaults)
-
-local treesitter_file_types = { "c", "lua", "typescript", "tsx" }
-require("nvim-treesitter").install(treesitter_file_types)
 vim.api.nvim_create_autocmd("FileType", {
 	pattern = treesitter_file_types,
 	callback = function(args)
@@ -65,18 +43,50 @@ vim.api.nvim_create_autocmd("FileType", {
 	end,
 })
 
-require('mason').setup()
-require('mason-lspconfig').setup()
-require('mason-tool-installer').setup({
-	ensure_installed = {
-		"lua_ls",
-		"stylua",
-		"vtsls"
-	}
+set_key("n", "<leader>d", vim.diagnostic.open_float, keymap_defaults)
+set_key("n", "<leader>ca", vim.lsp.buf.code_action, keymap_defaults)
+set_key("n", "<leader>f", vim.lsp.buf.format, keymap_defaults)
+set_key("n", "<leader>rn", vim.lsp.buf.rename, keymap_defaults)
+
+vim.pack.add({
+	"https://github.com/mason-org/mason.nvim",
+	"https://github.com/mason-org/mason-lspconfig.nvim",
+	"https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim",
+	"https://github.com/nvim-mini/mini.nvim",
+	"https://github.com/neovim/nvim-lspconfig",
+	"https://github.com/nvim-treesitter/nvim-treesitter",
+	"https://github.com/tpope/vim-sleuth",
+	-- copilot
+	"https://github.com/CopilotC-Nvim/CopilotChat.nvim",
+	"https://github.com/nvim-lua/plenary.nvim" -- dependency
 })
 
-require("vague").setup({
-	italic = false,
+require("mini.completion").setup()
+require("mini.icons").setup()
+require("mini.notify").setup()
+require("mini.surround").setup()
+
+require("mini.cmdline").setup({
+	autocomplete = {
+		enable = false,
+	},
+	autocorrect = {
+		enable = false
+	},
 })
 
-vim.cmd("colorscheme vague")
+-- require("mini.hues")
+-- vim.cmd("colorscheme miniwinter")
+
+local mini_pick = require("mini.pick")
+mini_pick.setup()
+set_key("n", "<leader>h", mini_pick.builtin.help, keymap_defaults)
+set_key("n", "\\", mini_pick.builtin.grep_live, keymap_defaults)
+set_key("n", ",", mini_pick.builtin.buffers, keymap_defaults)
+set_key("n", ";", mini_pick.builtin.files, keymap_defaults)
+
+require("nvim-treesitter").install(treesitter_file_types)
+
+require("mason").setup()
+require("mason-lspconfig").setup()
+require("mason-tool-installer").setup({ensure_installed = lsp_ensure_installed})
