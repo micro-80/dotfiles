6994efcff288fad82d1e2c886b0a576adf3de009
micro80 <me@micro80.com>
2026-02-09 09:58:38 +0000

    New init.el

--- common/.config/emacs/early-init.el
+++
@@ -1,5 +0,0 @@
-;; -*- lexical-binding: t; -*-
-(setq inhibit-startup-screen t)
-(push '(menu-bar-lines . 0) default-frame-alist)
-(push '(tool-bar-lines . 0) default-frame-alist)
-(push '(vertical-scroll-bars) default-frame-alist)
